3e42e3e91b098dc6b4e202623437d8a03fd474e3
niuniu315 <454378439@qq.com>
2022-07-05 00:18:51 +0800

    deploy

---
+++ precache-manifest.d110488f0d2ab3e872f9d888d4eda8b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c49edba03285491c1e2",
+    "url": "/simple-book-1/css/app.62469f2c.css"
+  },
+  {
+    "revision": "d28f18040198d795252f69d13bf50512",
+    "url": "/simple-book-1/index.html"
+  },
+  {
+    "revision": "5c49edba03285491c1e2",
+    "url": "/simple-book-1/js/app.df9495c1.js"
+  },
+  {
+    "revision": "3f24560e29e92bed5088",
+    "url": "/simple-book-1/js/chunk-vendors.91ad880f.js"
+  },
+  {
+    "revision": "3b39faaba0bb57fd48f8c4d8cd542d38",
+    "url": "/simple-book-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/simple-book-1/robots.txt"
+  }
+]);
